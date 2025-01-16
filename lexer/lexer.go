35ca31107813d21b42fe58bcf459c72927995f9d
Pavel Admaykin <padmaykin@ptsecurity.com>
2025-01-16 20:48:19 +0300

    feat: add golang realisation

---
+++ lexer/lexer.go
@@ -0,0 +1,304 @@
+package lexer
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Dor1ma/Strawberry/token"
+	"os"
+	"strconv"
+	"strings"
+	"text/scanner"
+	"unicode"
+)
+
+var eof = rune(-1)
+
+var (
+	// identifer error
+	errUnterminated = errors.New("unterminated string")
+	errEspace       = errors.New("invalid escape char")
+	errInvalidChar  = errors.New("invalid unicode char")
+
+	// number error
+	errLessPower = errors.New("power is required")
+)
+
+// Lexer represents a lexical scanner for Lox programing language.
+type Lexer struct {
+	s      *scanner.Scanner
+	ch     rune
+	tokBuf *strings.Builder
+}
+
+func (l *Lexer) consume() {
+	if l.isAtEnd() {
+		return
+	}
+	ch := l.s.Next()
+	if ch == scanner.EOF {
+		l.ch = eof
+		return
+	}
+	l.ch = ch
+}
+
+func (l *Lexer) peek() rune {
+	ch := l.s.Peek()
+	return ch
+}
+
+func (l *Lexer) skip() {
+	for unicode.IsSpace(l.ch) {
+		l.consume()
+	}
+}
+
+func (l *Lexer) isAtEnd() bool {
+	return l.ch == eof
+}
+
+func (l *Lexer) match(ch rune) bool {
+	l.consume()
+	if l.isAtEnd() || l.ch != ch {
+		return false
+	}
+	l.consume()
+	return true
+}
+
+func (l *Lexer) error(msg string) {
+	// FIXME: position is wrong
+	fmt.Fprintf(os.Stderr, "%s %s\n", l.Pos().String(), msg)
+}
+
+func (l *Lexer) readIdentifier() string {
+	l.tokBuf.Reset()
+	for isAlphaNumeric(l.ch) {
+		l.tokBuf.WriteRune(l.ch)
+		l.consume()
+	}
+	return l.tokBuf.String()
+}
+
+func (l *Lexer) readString() (string, error) {
+	l.tokBuf.Reset()
+	l.consume()
+	if l.ch == '"' {
+		l.consume()
+		return "", nil
+	}
+
+	for l.ch != '"' {
+		if l.isAtEnd() {
+			l.error(errUnterminated.Error())
+			return "", errUnterminated
+		} else if l.ch == '\\' {
+			peekCh := l.peek()
+			if peekCh == eof {
+				l.error(errEspace.Error())
+				return "", errEspace
+			}
+			l.consume()
+			switch peekCh {
+			case '"':
+				l.tokBuf.WriteRune('"')
+			case 'u':
+				code := make([]rune, 4)
+				for i := range code {
+					l.consume()
+					if !unicode.Is(unicode.Hex_Digit, l.ch) {
+						l.error(errInvalidChar.Error())
+						return "", errInvalidChar
+					}
+					code[i] = l.ch
+				}
+				l.tokBuf.WriteRune(charCode2Rune(string(code)))
+			}
+		} else {
+			l.tokBuf.WriteRune(l.ch)
+		}
+		l.consume()
+	}
+	// end ".
+	l.consume()
+	return l.tokBuf.String(), nil
+}
+
+func (l *Lexer) readNumber() (string, error) {
+
+	l.tokBuf.Reset()
+	for unicode.IsNumber(l.ch) {
+		l.tokBuf.WriteRune(l.ch)
+		l.consume()
+	}
+
+	if l.ch == '.' {
+		if !unicode.IsNumber(l.peek()) {
+			return l.tokBuf.String(), nil
+		}
+		l.tokBuf.WriteRune(l.ch)
+		l.consume()
+		for unicode.IsNumber(l.ch) {
+			l.tokBuf.WriteRune(l.ch)
+			l.consume()
+		}
+	}
+
+	if l.ch == 'E' || l.ch == 'e' {
+		seenPower := false
+		l.tokBuf.WriteRune(l.ch)
+		l.consume()
+		if l.ch == '+' || l.ch == '-' {
+			l.tokBuf.WriteRune(l.ch)
+			l.consume()
+		}
+		for unicode.IsNumber(l.ch) {
+			seenPower = true
+			l.tokBuf.WriteRune(l.ch)
+			l.consume()
+		}
+		if !seenPower {
+			l.error(errLessPower.Error())
+			return "", errLessPower
+		}
+	}
+
+	return l.tokBuf.String(), nil
+}
+
+// NextToken reads and returns token and literal.
+// It returns token.Illegal for invalid string or number.
+// It return token.EOF at the end of input string.
+func (l *Lexer) NextToken() (tok token.Token, literal string) {
+	l.skip()
+
+	switch l.ch {
+	case '(':
+		tok = token.LeftParen
+		literal = "("
+	case ')':
+		tok = token.RightParen
+		literal = ")"
+	case '{':
+		tok = token.LeftBrace
+		literal = "{"
+	case '}':
+		tok = token.RightBrace
+		literal = "}"
+	case ',':
+		tok = token.Comma
+		literal = ","
+	case '.':
+		tok = token.Dot
+		literal = "."
+	case '-':
+		tok = token.Minus
+		literal = "-"
+	case '+':
+		tok = token.Plus
+		literal = "+"
+	case ';':
+		tok = token.Semicolon
+		literal = ";"
+	case '/':
+		tok = token.Slash
+		literal = "/"
+	case '*':
+		tok = token.Star
+		literal = "*"
+	case '!':
+		if l.match('=') {
+			tok = token.BangEqual
+			literal = "!="
+		} else {
+			tok = token.Bang
+			literal = "!"
+		}
+		return
+	case '=':
+		if l.match('=') {
+			tok = token.EqualEqual
+			literal = "=="
+		} else {
+			tok = token.Equal
+			literal = "="
+		}
+		return
+	case '>':
+		if l.match('=') {
+			tok = token.GreaterEqual
+			literal = ">="
+		} else {
+			tok = token.Greater
+			literal = ">"
+		}
+		return
+	case '<':
+		if l.match('=') {
+			tok = token.LessEqual
+			literal = "<="
+		} else {
+			tok = token.Less
+			literal = "<"
+		}
+		return
+	case '"':
+		liter, err := l.readString()
+		if err != nil {
+			return token.Illegal, liter
+		}
+		tok = token.String
+		literal = liter
+		return
+	case eof:
+		tok = token.EOF
+		return
+	default:
+		if unicode.IsLetter(l.ch) {
+			literal = l.readIdentifier()
+			tok = token.Lookup(literal)
+			return
+		} else if unicode.IsNumber(l.ch) {
+			liter, err := l.readNumber()
+			if err != nil {
+				return token.Illegal, ""
+			}
+			tok = token.Number
+			literal = liter
+			return
+		}
+
+		tok = token.Illegal
+		literal = ""
+	}
+
+	l.consume()
+	return
+}
+
+// Pos returns current position of lexer.
+func (l *Lexer) Pos() scanner.Position {
+	return l.s.Pos()
+}
+
+func charCode2Rune(code string) rune {
+	v, err := strconv.ParseInt(code, 16, 32)
+	if err != nil {
+		return unicode.ReplacementChar
+	}
+	return rune(v)
+}
+
+func isAlphaNumeric(ch rune) bool { return unicode.IsLetter(ch) || unicode.IsNumber(ch) || ch == '_' }
+
+// New return an instance of Lexer.
+func New(input string) *Lexer {
+	s := &scanner.Scanner{}
+	s.Init(strings.NewReader(input))
+	l := &Lexer{
+		s:      s,
+		tokBuf: &strings.Builder{},
+	}
+	l.consume()
+	return l
+}
---
+++ resolver/resolver.go
@@ -0,0 +1,256 @@
+package resolver
+
+import (
+	"github.com/Dor1ma/Strawberry/ast"
+	"github.com/Dor1ma/Strawberry/errors"
+	"github.com/Dor1ma/Strawberry/token"
+)
+
+type functionType int
+
+const (
+	FunctionNone functionType = iota // not return
+	Function
+	Initializer
+	Method
+)
+
+type classType int
+
+const (
+	ClassNone classType = iota
+	Class
+)
+
+var (
+	scopes          = NewScopes()
+	curFunctionType = FunctionNone
+	curClassType    = ClassNone
+)
+
+func Resolve(node ast.Node) {
+	switch n := node.(type) {
+	default:
+		panic("Resolve failed: unknown ast type.")
+	case *ast.VariableExpr:
+		resolveVariableExpr(n)
+	case *ast.AssignExpr:
+		resolveAssignExpr(n)
+	case *ast.BinaryExpr:
+		resolveBinaryExpr(n)
+	case *ast.UnaryExpr:
+		resolveUnaryExpr(n)
+	case *ast.LogicalExpr:
+		resolveLogicalExpr(n)
+	case *ast.GroupingExpr:
+		resolveGroupExpr(n)
+	case *ast.CallExpr:
+		resolveCallExpr(n)
+	case *ast.GetExpr:
+		resolveGetExpr(n)
+	case *ast.SetExpr:
+		resolveSetExpr(n)
+	case *ast.ThisExpr:
+		resolveThisExpr(n)
+	case *ast.Literal:
+		// do nothing.
+	case *ast.BlockStmt:
+		resolveBlockStmt(n)
+	case *ast.VarStmt:
+		resolveVarStmt(n)
+	case *ast.FunctionStmt:
+		resolveFunctionStmt(n)
+	case *ast.ExprStmt:
+		resolveExprStmt(n)
+	case *ast.IfStmt:
+		resolveIfStmt(n)
+	case *ast.WhileStmt:
+		resolveWhileStmt(n)
+	case *ast.PrintStmt:
+		resolvePrintStmt(n)
+	case *ast.ReturnStmt:
+		resolveReturnStmt(n)
+	case *ast.ClassStmt:
+		resolveClassStmt(n)
+	}
+}
+
+func resolveVariableExpr(expr *ast.VariableExpr) {
+	if exist, init := scopes.check(expr.Name); exist && !init {
+		errors.Error(token.Identifier, "Cannot read local variable in its own initializer.")
+		return
+	}
+	resolveLocal(expr, expr.Name)
+}
+
+func resolveLocal(expr ast.Expr, name string) {
+	switch n := expr.(type) {
+	case *ast.VariableExpr:
+		// if variable doesn't exist in scopes, we regard it as a glabol variable.
+		for i := len(scopes) - 1; i >= 0; i-- {
+			if _, ok := scopes[i][name]; ok {
+				n.Distance = len(scopes) - 1 - i
+			}
+		}
+	case *ast.ThisExpr:
+		exist := false
+		for i := len(scopes) - 1; i >= 0; i-- {
+			if _, ok := scopes[i][name]; ok {
+				exist = true
+				break
+			}
+		}
+		if !exist {
+			errors.Error(token.This, "Cannot use 'this' outside of a class.")
+		}
+	}
+}
+
+func resolveAssignExpr(expr *ast.AssignExpr) {
+	Resolve(expr.Value)
+	resolveLocal(expr.Left, expr.Left.Name)
+}
+
+func resolveBinaryExpr(expr *ast.BinaryExpr) {
+	Resolve(expr.Left)
+	Resolve(expr.Right)
+}
+
+func resolveUnaryExpr(expr *ast.UnaryExpr) {
+	Resolve(expr.Right)
+}
+
+func resolveLogicalExpr(expr *ast.LogicalExpr) {
+	Resolve(expr.Left)
+	Resolve(expr.Right)
+}
+
+func resolveGroupExpr(expr *ast.GroupingExpr) {
+	Resolve(expr.Expression)
+}
+
+func resolveCallExpr(expr *ast.CallExpr) {
+	Resolve(expr.Callee)
+
+	for _, arg := range expr.Arguments {
+		Resolve(arg)
+	}
+}
+
+func resolveGetExpr(expr *ast.GetExpr) {
+	Resolve(expr.Object)
+}
+
+func resolveSetExpr(expr *ast.SetExpr) {
+	Resolve(expr.Object)
+	Resolve(expr.Value)
+}
+
+func resolveThisExpr(expr *ast.ThisExpr) {
+	if curClassType == ClassNone {
+		errors.Error(token.This, "Cannot use 'this' outside of a class.")
+		return
+	}
+	resolveLocal(expr, "this")
+}
+
+func resolveBlockStmt(block *ast.BlockStmt) {
+	scopes.begin()
+	resolveBlock(block.Statements)
+	scopes.end()
+}
+
+func resolveBlock(statements []ast.Stmt) {
+	for _, stmt := range statements {
+		Resolve(stmt)
+	}
+}
+
+func resolveVarStmt(stmt *ast.VarStmt) {
+	name := stmt.Name.Name
+	scopes.declare(name)
+	if stmt.Initializer != nil {
+		Resolve(stmt.Initializer)
+	}
+	scopes.define(name)
+}
+
+func resolveFunctionStmt(stmt *ast.FunctionStmt) {
+	scopes.declare(stmt.Name)
+	scopes.define(stmt.Name)
+	resolveFunction(stmt, Function)
+}
+
+func resolveFunction(function *ast.FunctionStmt, typ functionType) {
+	enclosingFunction := curFunctionType
+	curFunctionType = typ
+	defer func() {
+		curFunctionType = enclosingFunction
+	}()
+
+	scopes.begin()
+	for _, param := range function.Params {
+		scopes.declare(param.Name)
+		scopes.define(param.Name)
+	}
+	resolveBlock(function.Body)
+	scopes.end()
+}
+
+func resolveExprStmt(stmt *ast.ExprStmt) {
+	Resolve(stmt.Expression)
+}
+
+func resolveIfStmt(stmt *ast.IfStmt) {
+	Resolve(stmt.Condition)
+	Resolve(stmt.ThenBranch)
+	if stmt.ElseBranch != nil {
+		Resolve(stmt.ElseBranch)
+	}
+}
+
+func resolveWhileStmt(stmt *ast.WhileStmt) {
+	Resolve(stmt.Condition)
+	Resolve(stmt.Body)
+}
+
+func resolvePrintStmt(stmt *ast.PrintStmt) {
+	Resolve(stmt.Expression)
+}
+
+func resolveReturnStmt(stmt *ast.ReturnStmt) {
+	if curFunctionType == FunctionNone {
+		errors.Error(token.Return, "Cannot return from top-level code.")
+		return
+	}
+	if stmt.Value != nil {
+		if curFunctionType == Initializer {
+			errors.Error(token.Return, "Cannot return a value from an initializer.")
+			return
+		}
+		Resolve(stmt.Value)
+	}
+}
+
+func resolveClassStmt(stmt *ast.ClassStmt) {
+	scopes.declare(stmt.Name)
+	scopes.define(stmt.Name)
+
+	enclosingClass := curClassType
+	curClassType = Class
+	defer func() {
+		curClassType = enclosingClass
+	}()
+
+	scopes.begin()
+	scopes.declare("this")
+	scopes.define("this")
+	for _, method := range stmt.Methods {
+		typ := Method
+		if method.IsInitializer {
+			typ = Initializer
+		}
+		resolveFunction(method, typ)
+	}
+	scopes.end()
+}
